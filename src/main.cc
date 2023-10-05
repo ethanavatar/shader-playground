af9ab863297d36361decfe931de3fcbb0d8f85c5
Ethan Evans <ethanalexevans@gmail.com>
2023-10-04 17:29:37 -0700

    Rectangle example

--- src/main.cc
+++ src/main.cc
@@ -7,26 +7,9 @@
 
 #include <glad/glad.h>
 
-
-static void error_callback(int32_t error, const char* description) {
-    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
-}
-
-static void process_input(GLFWwindow *const window, int32_t key, int32_t scancode, int32_t action, int32_t mods) {
-    (void) mods;
-    (void) scancode;
-
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-}
-
-static void process_resize(GLFWwindow *const window, int32_t width, int32_t height) {
-    (void) window;
-
-    glViewport(0, 0, width, height);
-}
-
+static void error_callback(int32_t error, const char* description);
+static void process_input(GLFWwindow *const window, int32_t key, int32_t scancode, int32_t action, int32_t mods);
+static void process_resize(GLFWwindow *const window, int32_t width, int32_t height);
 
 static const char *vertex_shader_source = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -91,19 +74,28 @@ int32_t main(void) {
     glDeleteShader(fragment_shader);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
     };
 
-    uint32_t vbo, vao;
+    uint32_t indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    }; 
+
+    uint32_t vbo, vao, ebo;
     glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
     
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
@@ -126,7 +118,7 @@ int32_t main(void) {
         // Draw stuff
         glUseProgram(shader_program);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         // Push stuff
@@ -142,5 +134,25 @@ int32_t main(void) {
     glfwTerminate();
 
     exit(EXIT_SUCCESS);
+}
+
+
+static void error_callback(int32_t error, const char* description) {
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+static void process_input(GLFWwindow *const window, int32_t key, int32_t scancode, int32_t action, int32_t mods) {
+    (void) mods;
+    (void) scancode;
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+static void process_resize(GLFWwindow *const window, int32_t width, int32_t height) {
+    (void) window;
+
+    glViewport(0, 0, width, height);
 }
 
