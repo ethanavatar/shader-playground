ecf03b98561199448103af80b4ebc9118326ab9f
Ethan Evans <ethanalexevans@gmail.com>
2023-10-11 23:14:55 -0700

    Cleaned up time.cpp

--- src/time.cpp
+++ src/time.cpp
@@ -1,23 +1,19 @@
-#if __WIN32__
-    #include <__msvc_chrono.hpp>
-#endif // __WIN32__
-
 #include <stdint.h>
 
 #if !defined(_WIN32) && !defined(_WIN64) // Linux - Unix
     #include <sys/time.h>
     typedef timeval sys_time_t;
-    inline void system_time(sys_time_t* t) {
+    inline void system_time(sys_time_t *t) {
         gettimeofday(t, NULL);
     }
-    inline uint64_t time_to_msec(const sys_time_t& t) {
+    inline uint64_t time_to_msec(const sys_time_t &t) {
         return t.tv_sec * 1000LL + t.tv_usec / 1000;
     }
 #else // Windows and MinGW
     #include <sys/timeb.h>
     typedef _timeb sys_time_t;
     inline void system_time(sys_time_t* t) { _ftime(t); }
-    inline uint64_t time_to_msec(const sys_time_t& t) {
+    inline uint64_t time_to_msec(const sys_time_t &t) {
         return t.time * 1000LL + t.millitm;
     }
 #endif
